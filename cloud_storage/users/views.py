57af8e6a02024b8b00a2ce94c2a21a20b9724e07
Vladislav Luchnikov <whatislov@mail.ru>
2025-01-05 17:39:27 +0300

    feat: add login, logout and registration forms

---
+++ cloud_storage/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+from .forms import RegisterForm
+
+
+class RegistrationPageView(TemplateView):
+    template_name = "registration/register.html"
+
+    def get(self, request, *args, **kwargs):
+        register_form = RegisterForm()
+        return render(request, self.template_name, {"form": register_form})
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            new_user = register_form.save(commit=False)
+            new_user.save()
+            return redirect("users:login")
+
+        return render(request, self.template_name, {"form": register_form})
