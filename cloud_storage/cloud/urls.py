ff047d61970a491c564a8f9b26aa2fbe63854139
Vladislav Luchnikov <whatislov@mail.ru>
2025-01-24 23:53:06 +0300

    refactor: added new paths for s3 object handling view

--- cloud_storage/cloud/urls.py
+++ cloud_storage/cloud/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import MainPageView
+from .views.main_view import MainPageView
+from .views.object_handling_view import S3ObjectHandlingView
 
 
 urlpatterns = [
     path("", MainPageView.as_view(), name="main"),
+    path("create_object/", S3ObjectHandlingView.as_view(), name="create_object"),
+    path("delete_object/", S3ObjectHandlingView.as_view(), name="delete_object"),
+    path("update_object/", S3ObjectHandlingView.as_view(), name="update_object"),
     # path('search/', SearchPageView.as_view(), name='search')
 ]
