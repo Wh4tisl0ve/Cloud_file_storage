da260d7c7ca73bdec83fa38e3c392154185ba26f
Vladislav Luchnikov <whatislov@mail.ru>
2025-01-24 23:48:24 +0300

    feat: added main view for handling main actions

---
+++ cloud_storage/cloud/views/main_view.py
@@ -0,0 +1,40 @@
+import io
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.apps import apps
+
+
+class MainPageView(LoginRequiredMixin, TemplateView):
+    def get(self, request, *args, **kwargs):
+        s3_service = apps.get_app_config("cloud").s3_service
+
+        current_path = request.GET.get("path")
+
+        user_objects = sorted(
+            s3_service.get_objects(request.user.id, current_path),
+            key=lambda x: not x.is_dir,
+        )
+
+        return render(
+            request, "cloud/layouts/index.html", context={"user_objects": user_objects}
+        )
+
+    def post(self, request, *args, **kwargs):
+        s3_service = apps.get_app_config("cloud").s3_service
+
+        files = zip(
+            request.FILES.getlist("fileList"),
+            request.POST.getlist("filePaths"),
+        )
+
+        current_path = request.GET.get("path")
+
+        for file, full_path in files:
+            s3_service.create_object(
+                request.user.id, full_path, current_path, io.BytesIO(file.read())
+            )
+
+        return redirect("cloud:main")
