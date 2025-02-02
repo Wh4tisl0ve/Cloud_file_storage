dbd8bd1c82c2ed8846a233899a8c10cece8ab922
Vladislav Luchnikov <whatislov@mail.ru>
2025-02-02 18:20:51 +0300

    refactor: added all url

--- cloud_storage/cloud/urls.py
+++ cloud_storage/cloud/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
-from .views.main_view import MainPageView
+from .views.main_page_view import MainPageView
+from .views.search_page_view import SearchPageView
+
 from .views.object_handling_view import S3ObjectHandlingView
 
 
 urlpatterns = [
     path("", MainPageView.as_view(), name="main"),
+    path("upload_object/", MainPageView.as_view(), name="upload_object"),
     path("create_object/", S3ObjectHandlingView.as_view(), name="create_object"),
     path("delete_object/", S3ObjectHandlingView.as_view(), name="delete_object"),
-    path("update_object/", S3ObjectHandlingView.as_view(), name="update_object"),
-    # path('search/', SearchPageView.as_view(), name='search')
+    path("rename_object/", S3ObjectHandlingView.as_view(), name="rename_object"),
+    path("download_object/", S3ObjectHandlingView.as_view(), name="download_object"),
+    path("search/", SearchPageView.as_view(), name="search"),
 ]
