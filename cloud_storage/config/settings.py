68c793192b07ead021afa40b06bd24b82620daef
Vladislav Luchnikov <whatislov@mail.ru>
2025-02-04 16:54:49 +0300

    refactor: update template

--- cloud_storage/config/settings.py
+++ cloud_storage/config/settings.py
@@ -96,9 +96,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = 'ru-RU'
+TIME_ZONE = 'Europe/Moscow'
+USE_L10N = True
 
 USE_I18N = True
 
